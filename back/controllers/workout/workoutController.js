ed3525722f7b5a5a91e89e2ab69aa533afa26270
startender <mathsitedu@mail.ru>
2022-09-10 00:13:29 +0300

    get workout

--- back/controllers/workout/workoutController.js
+++ back/controllers/workout/workoutController.js
@@ -17,3 +17,18 @@ export const addNewWorkout = asyncHandler(async (req, res) => {
 
   res.json(workout)
 })
+
+// @desc    
+// @route POST /api/workouts
+// @access Private
+
+export const getWorkout = asyncHandler(async (req, res) => {
+  const workout = await Workout.findById(req.params.id)
+    .populate('exercises')
+    .lean()
+
+  const minutes = Math.ceil(workout.exercises.length * 3.7)
+
+
+  res.json({...workout, minutes})
+})
--- back/routes/workoutRoutes.js
+++ back/routes/workoutRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addNewWorkout } from '../controllers/workout/workoutController.js'
+import { addNewWorkout, getWorkout } from '../controllers/workout/workoutController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, addNewWorkout)
+router.route('/:id').get(protect, getWorkout)
 
 export default router
