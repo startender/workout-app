76e5d1c88f4bb98e6071097152143106320a0884
startender <mathsitedu@mail.ru>
2022-09-09 22:27:30 +0300

    exercise and workout models with log in

--- back/models/exerciseModel.js
+++ back/models/exerciseModel.js
@@ -7,8 +7,8 @@ const exerciseSchema = mongoose.Schema(
       type: Number,
       required: true,
     },
-    image: {
-      type: String,
+    imageId: {
+      type: Number,
       required: true,
     },
   },
