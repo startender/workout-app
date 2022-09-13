b171024116570de3a62b835d68000d7177531b7f
startender <mathsitedu@mail.ru>
2022-09-13 23:14:11 +0300

    hooks and some components

---
+++ front/src/components/pages/newWorkout/newWorkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactSelect from 'react-select'
+import { Link } from 'react-router-dom'
+import Layout from "../../common/Layout";
+
+import bgImage from '../../../images/new-workout-bg.jpeg'
+
+import Field from "../../ui/Field/Field";
+import Button from "../../ui/Button/Button";
+import Alert from "../../ui/Alert/Alert";
+import Loader from "../../ui/Loader";
---
+++ front/src/hooks/useAuth.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext"
+
+export const useAuth = () => {
+  const { isAuth, setIsAuth } = useContext(AuthContext)
+  return {
+    isAuth,
+    setIsAuth,
+  }
+}
